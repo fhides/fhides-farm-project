e539f4f859f8a219dca5ebf759daa8587ab1a968
fhidesjoseph <fhidesjoseph@gmail.com>
2022-06-25 02:38:44 +0300

    1 commit

---
+++ assets/js/main.js
@@ -0,0 +1,69 @@
+document.addEventListener('DOMContentLoaded',()=>{
+
+    fetchData()
+    //call dom elements
+    let form =document.getElementById('order-form');
+    let orderBtn=document.querySelectorAll('.btn-pry');
+    let close=document.getElementById('close');
+    let email=document.getElementById('email').value;
+    
+    //close event
+    close.addEventListener('click',()=>{
+        form.style.display="none"
+    })
+
+    //order button event
+    orderBtn.forEach(btn => {
+
+    btn.addEventListener('click', event => {
+        form.style.display="block";
+    });
+    form.addEventListener('submit',()=>{
+        //check if eamilfield is empty
+        if(email==""){
+            alert("email required")
+        }
+    })
+ 
+ });
+})
+//fetch data from api
+function fetchData(){
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '99b1489bc1mshe4869d13ccb8365p164db9jsn74e1339350c1',
+		'X-RapidAPI-Host': 'edamam-food-and-grocery-database.p.rapidapi.com'
+	}
+};
+
+fetch('https://edamam-food-and-grocery-database.p.rapidapi.com/parser?ingr=apple', options)
+	.then(response => response.json())
+	.then(response => {
+        console.log(response.hints)
+        //appendCard(response)
+    })
+	.catch(err => console.error(err));
+}
+const produce=document.getElementById('produce');
+
+function appendCard(response) {
+    response.hints.forEach((items) => {
+      let list = document.createElement("li");
+  
+      list.innerHTML = `
+      <div class=card>
+      <div class="img">
+            <figure>
+        <img src=${items.image} alt="products">
+            <figcaption>
+            ${items.label}
+            </figcaption>
+         </figure>
+        </div>
+        <button id="order" class="btn-pry">Order now</button>
+      </div>`
+      
+      produce.appendChild(list);
+    });
+}
